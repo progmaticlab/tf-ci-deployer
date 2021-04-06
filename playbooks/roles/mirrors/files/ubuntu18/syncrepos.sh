6a84dc89639a34d5428e7f88e5cea76eb764cbcf
Gleb Galkin <gleb108@gmail.com>
2021-04-06 16:49:01 +0300

    ubuntu18: adding LXD images to mirror

--- playbooks/roles/mirrors/files/ubuntu18/syncrepos.sh
+++ playbooks/roles/mirrors/files/ubuntu18/syncrepos.sh
@@ -9,3 +9,7 @@ cd ${MIRRORDIR}/ubuntu18
 sed -i "s|%MIRRORDIR%|${MIRRORDIR}/ubuntu18/${DATE}|" /etc/apt/mirror.list
 apt-mirror && (rm -f stage; ln -s ${DATE} stage)
 
+#Downloading LXD images for juju
+mkdir ${DATE}/lxd
+cd ${DATE}/lxd
+wget -q https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-lxd.tar.xz https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-root.tar.xz
